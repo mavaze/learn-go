f58b7d5d29ce47daad6203b037d5cec001bb9e4c
mavaze <mavaze@users.noreply.github.com>
2021-12-27 12:20:59 +0530

    leetcode challenges

---
+++ challenges/leetcode_test.go
@@ -0,0 +1,122 @@
+package challenges
+
+import "testing"
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+func TestBestTimeToBuyAndSellStock(t *testing.T) {
+	type Results struct {
+		expected int
+		input    []int
+	}
+	testCases := []Results{
+		{5, []int{7, 1, 5, 3, 6, 4}},
+		{0, []int{7, 6, 4, 3, 1}},
+		{3, []int{7, 8, 4, 7, 1}},
+		{1, []int{7, 8, 4, 3, 1}},
+		{4, []int{3, 3, 5, 0, 0, 3, 1, 4}},
+	}
+
+	// maxProfit function is actual implementation
+	maxProfit := func(prices []int) int {
+		var profit int
+		var min int = 10001
+		for i := 0; i < len(prices); i++ {
+			if prices[i] < min {
+				min = prices[i]
+			}
+			if profit < prices[i]-min {
+				profit = prices[i] - min
+			}
+		}
+		return profit
+	}
+
+	for i, tc := range testCases {
+		actual := maxProfit(tc.input)
+		if actual != tc.expected {
+			t.Errorf("Tesstcase [%d] failed, Expected [%d] but wrongly calculated to [%d]", i, tc.expected, actual)
+		}
+	}
+}
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee
+// Find the maximum profit you can achieve with one or multiple transactions considering fee per each transaction.
+// Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+func TestBestTimeToBuyAndSellStockWithFee(t *testing.T) {
+	type Results struct {
+		expected int
+		input    []int
+		fee      int
+	}
+	testCases := []Results{
+		{8, []int{1, 3, 2, 8, 4, 9}, 2},
+		{6, []int{1, 3, 7, 5, 10, 3}, 3},
+		{7, []int{1, 3, 6, 7, 5, 10, 3}, 2},
+	}
+
+	// maxProfitWithFee function is actual implementation
+	maxProfitWithFee := func(prices []int, fee int) int {
+		var profit int
+		var diff int = prices[0]
+
+		for i := 1; i < len(prices); i++ {
+			diff = min(diff, prices[i]-profit)
+			profit = max(profit, prices[i]-diff-fee)
+		}
+		return profit
+	}
+
+	for i, tc := range testCases {
+		actual := maxProfitWithFee(tc.input, tc.fee)
+		if actual != tc.expected {
+			t.Errorf("Tesstcase [%d] failed, Expected [%d] but wrongly calculated to [%d]", i, tc.expected, actual)
+		}
+	}
+}
+
+// https://leetcode.com/problems/house-robber
+// Each house has a certain amount of money stashed. You have to rob maximum money provided you won't alert police by robbing 2 adjacent houses.
+func TestHouseRobber(t *testing.T) {
+	type Results struct {
+		expected int
+		input    []int
+	}
+	testCases := []Results{
+		{6, []int{3, 2, 2, 3}},
+		{4, []int{1, 2, 3, 1}},
+		{12, []int{2, 7, 9, 3, 1}},
+		{20, []int{1, 3, 6, 7, 5, 10, 3}},
+	}
+
+	// rob function is actual implementation
+	rob := func(nums []int) int {
+		var output []int = make([]int, len(nums)+2)
+		for i := len(nums) - 1; i >= 0; i-- {
+			output[i] = max(output[i+2]+nums[i], output[i+1])
+		}
+		return output[0]
+	}
+
+	for i, tc := range testCases {
+		actual := rob(tc.input)
+		if actual != tc.expected {
+			t.Errorf("Tesstcase [%d] failed, Expected [%d] but wrongly calculated to [%d]", i, tc.expected, actual)
+		}
+	}
+}
+
+var min = func(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+var max = func(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
